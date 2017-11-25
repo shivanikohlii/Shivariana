11464792e590d9b401ffdd393259f79677572661
Shivani Kohli <shivanikohli@Shivanis-MacBook-Air.local>
2017-11-25 00:05:42 -0600

    message handling

--- index.js
+++ index.js
@@ -80,6 +80,12 @@ app.post('/webhook', (req, res) => {
     // Get the sender PSID
     let sender_psid = webhook_event.sender.id;
     console.log('Sender PSID: ' + sender_psid);
+
+    if(webhook_event.message){
+      handleMessage(sender_psid, webhook_event.message);
+    }else if(webhook_event.postbook){
+      handlePostback(sender_psid, webhook_event.postback);
+    }
     });
 
     // Returns a '200 OK' response to all requests
@@ -92,15 +98,37 @@ app.post('/webhook', (req, res) => {
 });
    // Handles messages events
 function handleMessage(sender_psid, received_message) {
-  
+  if(received_message.text){
+    response = { 
+      "text": 'You sent the message: "${received_message.text}". Now send me an image!'
+    }
   }
+  //sends the response message
+  function callSendAPI(sender_psid, response) {
+    // Construct the message body
+    let request_body = {
+      "recipient": {
+        "id": sender_psid
+      },
+      "message": response
+    }
+   // Send the HTTP request to the Messenger Platform
+   request({
+    "uri": "https://graph.facebook.com/v2.6/me/messages",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('message sent!')
+    } else {
+      console.error("Unable to send message:" + err);
+    }
+  }); 
+}
   
   // Handles messaging_postbacks events
   function handlePostback(sender_psid, received_postback) {
   
   }
-  
-  // Sends response messages via the Send API
-  function callSendAPI(sender_psid, response) {
-    
-  }
+}
