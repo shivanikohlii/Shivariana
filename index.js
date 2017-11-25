d354684f1ac31202213209dd5722d6eeb94e5053
Shivani Kohli <shivanikohli@Shivanis-MacBook-Air.local>
2017-11-25 00:31:46 -0600

    sup

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const 
   express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
-
+  app = express().use(bodyParser.json()), // creates express http server
+  request = require('request');
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
