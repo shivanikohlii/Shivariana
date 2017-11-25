422e7ee8e9fd278f78bcbf6a56616b6b8163cfcd
Shivani Kohli <shivanikohli@Shivanis-MacBook-Air.local>
2017-11-24 23:46:26 -0600

    More webhook stuff

--- index.js
+++ index.js
@@ -62,3 +62,45 @@ app.post('/webhook', (req, res) => {
        }
      }
    });
+   // Creates the endpoint for our webhook 
+app.post('/webhook', (req, res) => {  
+ 
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+     // Gets the body of the webhook event
+    let webhook_event = entry.messaging[0];
+    console.log(webhook_event);
+
+    // Get the sender PSID
+    let sender_psid = webhook_event.sender.id;
+    console.log('Sender PSID: ' + sender_psid);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+});
+   // Handles messages events
+function handleMessage(sender_psid, received_message) {
+  
+  }
+  
+  // Handles messaging_postbacks events
+  function handlePostback(sender_psid, received_postback) {
+  
+  }
+  
+  // Sends response messages via the Send API
+  function callSendAPI(sender_psid, response) {
+    
+  }
