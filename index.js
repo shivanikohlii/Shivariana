0af4b96ea0deee06d5745323fc045290aca6454b
Shivani Kohli <shivanikohli@Shivanis-MacBook-Air.local>
2017-11-25 16:39:43 -0600

    GIF stuff

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ function handleMessage(sender_psid, received_message) {
 function createMessage(sender_psid, keyphrase) {
   var giphyLink = "http://api.giphy.com/v1/gifs/search?q="+keyphrase+"&api_key="+giphyKey+"&limit=1";
   request(giphyLink, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+    if (!error && response.meta.status == 200) {
       var imgURL = JSON.parse(body).data.image_url;
       if (imgURL) {
          var response_message = {
