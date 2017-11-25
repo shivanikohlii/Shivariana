8ca292a5aac99439ab7dc8c6a0a8f46fd59754a2
Shivani Kohli <shivanikohli@Shivanis-MacBook-Air.local>
2017-11-25 17:17:32 -0600

    aoduhajf2322

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ function createMessage(sender_psid, keyphrase) {
     //console.log(response);
     if (!error) {
       console.log("[1/2] Response from giphy received");
-      var imgURL = JSON.parse(body).data[0].original.url;
+      var imgURL = JSON.parse(body).data[0].images.original.url;
       console.log(imgURL);
       if (imgURL) {
         var response_message = {
