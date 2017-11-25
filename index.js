e6e06f1d34034264252e61a881c511d266aae350
Shivani Kohli <shivanikohli@Shivanis-MacBook-Air.local>
2017-11-25 17:23:52 -0600

    rand3

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ function handleMessage(sender_psid, received_message) {
   }); 
 }
 function createMessage(sender_psid, keyphrase) {
-  var giphyLink = "http://api.giphy.com/v1/gifs/search?q="+keyphrase+"&api_key="+giphyKey+"&limit=1";
+  var giphyLink = "http://api.giphy.com/v1/gifs/search?q="+keyphrase+"&api_key="+giphyKey;
   request(giphyLink, function (error, response, body) {
     //console.log(response);
     if (!error) {
