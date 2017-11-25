c61b6cec4fff8a4173d426b85ac098c088c21392
Shivani Kohli <shivanikohli@Shivanis-MacBook-Air.local>
2017-11-24 23:39:08 -0600

    Webhook done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Imports dependencies and set up http server
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const 
   express = require('express'),
   bodyParser = require('body-parser'),
